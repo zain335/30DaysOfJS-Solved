ab7352b757da08afbb6161cd107b807458c23ec9
Muhammd Zain Ali <63092772+zain335@users.noreply.github.com>
2023-01-11 16:47:24 +0500

    Add files via upload

---
+++ Day01/datatypes.js
@@ -0,0 +1,21 @@
+
+console.log(typeof 'zain ali')
+console.log(typeof True)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 23)
+
+let var1
+let var2
+let var3
+let var4
+
+
+let firstName='zain'
+let lastName='ali'
+let maritalStatus='single'
+let country='Pakistan'
+let age=23
+
+let [first_name,last_name,marital_staus,_country,_age] = ['zain','ali','single','Pakistan',23]
+
---
+++ Day01/variable.js
@@ -0,0 +1,10 @@
+// comments can make code readable
+// Welcom to 30DaysOfJavaScript
+/**
+ * comments can make code readable, eay to reuse and informative
+ */
+
+let name='zain ali';
+let isSingle = True;
+let undefine = undefined;
+let nullVar = null;
